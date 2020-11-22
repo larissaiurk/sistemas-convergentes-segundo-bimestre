a72c85dd934a304879ef0cffeb06ccf642746076
Larissa <larissa.iurk@gmail.com>
2020-11-22 13:47:12 -0300

    init projects

---
+++ crud-um/app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+const cors = require('cors');
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://development:S7FOgGx5YFsUKJfB@cluster0.j9yts.mongodb.net/sistemasconvergentes?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(()=> {
+  console.log('Connection OK');
+}).catch((err) => {
+  console.log(err);
+})
+
+var app = express();
+
+app.use(cors())
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+const verifyAccessToken = require('./routes/middleware/verifyAccessTokenMiddleware')
+app.use('/', verifyAccessToken, indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
---
+++ crud-um/routes/middleware/verifyAccessTokenMiddleware.js
@@ -0,0 +1,23 @@
+const Users = require('../../models/users');
+const jwt = require('jsonwebtoken');
+
+module.exports = async (req, res, next) => {
+  try {
+    if(!req.headers['authorization']) {
+      return res.status(400).json({msg: 'Access token not informed...'})
+    }
+
+    const token = req.headers['authorization'].split(' ')
+    if(token[0] !== 'Bearer'){
+      return res.status(400).json({msg: 'Access token not valid...'})
+    }
+
+    const user = await jwt.verify(token[1], 'TOP_SECRET')
+    req.body.user = await Users.findOne({username: user.username})
+
+    next()
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({msg: 'Error during token validation...', error: error})
+  }
+}
