a4a4e89e0a09570befb2bcf3f704f35c94212915
Larissa <larissa.iurk@gmail.com>
2020-11-25 00:47:53 -0300

    product feat: add put and delete for products api

--- crud-dois/routes/products.js
+++ crud-dois/routes/products.js
@@ -18,13 +18,13 @@ router.get('/', async function(req, res, next) {
     //     msg: 'User don\'t have access in this route... ',
     //   }) 
     // }
-    // const users = await Users.find()
-    return res.status(200).json('oi')
+    const products = await Products.find()
+    return res.status(200).json(products)
 
   } catch (error) {
     console.log(error);
     return res.status(500).json({
-      msg: 'Users cannot be listed...',
+      msg: 'Products cannot be listed...',
       error
     }) 
   }
@@ -34,16 +34,29 @@ router.get('/', async function(req, res, next) {
 router.post('/', async function(req, res, next) {
   try {
 
-    const options = {
-      url: 'http://localhost:3000/users',
-      headers: {
-        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Ik1hcmlhIiwicGVybWlzc2lvbnMiOlsiVVNFUl9BRE1JTiIsIlBST0RVQ1RfQURNSU4iLCJDQVRFR09SWV9BRE1JTiJdLCJpYXQiOjE2MDYxNzEzODksImV4cCI6MTYwNjI1Nzc4OX0.3Uf6AQcZC9jCoTYCOAAlPObArdwwIH2R6mqBc22s6bw'
-      }
-    };
-
-    request(options, (error, resp, body) => {
-      console.log(resp.data, body);
-    })
+    // const options = {
+    //   url: 'http://localhost:3000/users',
+    //   headers: {
+    //     'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Ik1hcmlhIiwicGVybWlzc2lvbnMiOlsiVVNFUl9BRE1JTiIsIlBST0RVQ1RfQURNSU4iLCJDQVRFR09SWV9BRE1JTiJdLCJpYXQiOjE2MDYxNzEzODksImV4cCI6MTYwNjI1Nzc4OX0.3Uf6AQcZC9jCoTYCOAAlPObArdwwIH2R6mqBc22s6bw'
+    //   }
+    // };
+    // try {
+    //   request(options, (error, resp, body) => {
+    //     if(resp){
+    //       console.log(resp.data, body);
+    //     } else {
+    //       return res.status(500).json({
+    //         msg: 'User service is down...',
+    //         error
+    //       })
+    //     }
+    //   })  
+    // } catch (error) {
+    //   return res.status(500).json({
+    //     msg: 'New product cannot be created...',
+    //     error
+    //   }) 
+    // }
 
     const newProduct = new Products({
       name: req.body.name,
@@ -77,6 +90,60 @@ router.post('/image', multer(multerConfig).single('file'), async (req, res, next
   console.log(post);
   return res.json(post)
 
+});
+
+
+/* PUT users listing. */
+router.put('/:id', async function(req, res, next) {
+  try {
+
+    const { id } = req.params
+    const { description, price } = req.body
+
+    const product = await Products.findById({_id: id})
+    if(!product){
+      return res.status(400).json({
+        msg: 'Unable find product...'
+      })
+    }
+
+    product.description = description
+    product.price = price
+
+    await product.save()
+    return res.status(200).json('Product successfully updated!')
+    
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      msg: 'Product cannot be updated...',
+      error
+    }) 
+  }
+});
+
+
+/* DELETE users listing. */
+router.delete('/:id', async function(req, res, next) {
+  try {
+    const { id } = req.params
+
+    const product = await Products.deleteOne({_id: id})
+    if(product.deletedCount === 0){
+      return res.status(400).json({
+        msg: 'Unable find product...'
+      })
+    }
+    
+    return res.status(200).json('Product successfully deleted!')
+    
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      msg: 'Product cannot be deleteded...',
+      error
+    }) 
+  }
 });
 
 
